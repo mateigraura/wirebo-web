de4dbff5ed54057963bd5a3d401feb317ffa23b9
mateigraura <grauramatei@gmail.com>
2020-12-04 19:46:27 +0200

    init

---
+++ README.md
@@ -0,0 +1,2 @@
+# wirebo-web
+
---
+++ src/pages/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+interface Props {
+}
+
+const App = (props: Props) => {
+  return (
+    <div>
+      <header>
+        <h1>
+          Wirebo
+        </h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
